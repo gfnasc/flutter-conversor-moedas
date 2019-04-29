42e482fd7cac2879a7185a352a4e05b40e901927
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2019-04-29 15:03:24 -0300

    consumindo api, await/async, Future e FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart' as async;
+import 'dart:convert';
 
-void main(){
+const request = "https://api.hgbrasil.com/finance?format=json&key=e45c5869";
+
+void main() async {
 
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando dados...",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25
+                    ),
+                    textAlign: TextAlign.center
+                  )
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                      child: Text(
+                          "Erro ao carregar dados :(",
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 25
+                          ),
+                          textAlign: TextAlign.center
+                      )
+                  );
+                } else {
+                  return Container(color: Colors.green);
+                }
+            }
+          }
+      ),
+    );
+  }
 }
